8034ee5e8aa0cc64222989ffef3cdd12c9427b94
André Meneses <andre_meneses2001@hotmail.com>
2023-06-22 17:12:26 -0300

    Almost ready

---
+++ dashboard.py
@@ -0,0 +1,133 @@
+import streamlit as st
+import plot_runner as pr
+import data_handler as dh
+
+def streamlit_grade_plot():
+
+    var_dict = {'Matemática':'NU_NOTA_MT','Ciências das Natureza':'NU_NOTA_CN','Ciências Humanas':'NU_NOTA_CH','Linguagens':'NU_NOTA_LC', 'Redação':'NU_NOTA_REDACAO'}
+
+    col1, col2= st.columns(2)
+
+    variables = [key for key in var_dict.keys()]
+    anos = ['2016', '2017','2018','2019','2020']
+
+    var = col1.selectbox('Selecione a prova', variables)
+    var = var_dict[var]
+
+    ano = col2.select_slider('Selecione o ano', anos)
+
+    df = dh.read_enem('dados/enem_' + ano + '.csv')
+
+    map_plot = pr.plot_geomap(df, var)
+    dist_plot = pr.plot_dist(df, var)
+
+    st.plotly_chart(map_plot)
+
+    st.write("""### Distribuição das notas""")
+    st.pyplot(dist_plot.figure)
+
+
+def streamlit_categorical_plot():
+
+    tp_cor_raca_dict = {
+    '0': 'Não declarado',
+    '1': 'Branca',
+    '2': 'Preta',
+    '3': 'Parda',
+    '4': 'Amarela',
+    '5': 'Indígena',
+    '6': 'Não dispõe da informação'
+    }
+
+    tp_faixa_etaria_dict = {
+    '1': 'Menor de 17 anos',
+    '2': '17 anos',
+    '3': '18 anos',
+    '4': '19 anos',
+    '5': '20 anos',
+    '6': '21 anos',
+    '7': '22 anos',
+    '8': '23 anos',
+    '9': '24 anos',
+    '10': '25 anos',
+    '11': 'Entre 26 e 30 anos',
+    '12': 'Entre 31 e 35 anos',
+    '13': 'Entre 36 e 40 anos',
+    '14': 'Entre 41 e 45 anos',
+    '15': 'Entre 46 e 50 anos',
+    '16': 'Entre 51 e 55 anos',
+    '17': 'Entre 56 e 60 anos',
+    '18': 'Entre 61 e 65 anos',
+    '19': 'Entre 66 e 70 anos',
+    '20': 'Maior de 70 anos'
+    }
+
+    var_dict = {'Faixa Etária':'TP_FAIXA_ETARIA','Cor':'TP_COR_RACA'}
+    dicts = {'TP_FAIXA_ETARIA':tp_faixa_etaria_dict, 'TP_COR_RACA':tp_cor_raca_dict}
+
+    col1, col2= st.columns(2)
+
+    variables = [key for key in var_dict.keys()]
+    anos = ['2016', '2017','2018','2019','2020']
+
+    var = col1.selectbox('Selecione a variável', variables)
+    var = var_dict[var]
+
+    ano = col2.select_slider('Selecione o ano', anos)
+
+    df = dh.read_enem('dados/enem_' + ano + '.csv')
+
+    map_plot = pr.plot_geomap(df, var, cat=True)
+    dist_plot = pr.plot_dist(df, var)
+
+    st.plotly_chart(map_plot)
+    
+    st.write("""#### Legenda""")
+    
+    for key,item in dicts[var].items():
+        st.write("- " , key , " : " , item)
+
+
+def streamlit_income_plot():
+    var_dict = {'Renda':'Q006'}
+    # dicts = {'Q006':tp_faixa_etaria_dict, 'TP_ESCOLA':tp_cor_raca_dict}
+
+    col1, col2= st.columns(2)
+
+    variables = [key for key in var_dict.keys()]
+    anos = ['2016', '2017','2018','2019','2020']
+
+    var = col1.selectbox('Selecione a variável', variables)
+    var = var_dict[var]
+
+    ano = col2.select_slider('Selecione o ano', anos, key='aaa')
+
+    df = dh.read_enem('dados/enem_' + ano + '.csv')
+
+    map_plot = pr.plot_geomap(df, var)
+
+    renda_plot = pr.plot_violin(df, 'Q006')
+
+    st.plotly_chart(map_plot)
+
+    st.write("""### Violin Plot""")
+
+    st.pyplot(renda_plot.figure)
+
+def main():
+    st.write("""# Microdados ENEM 2016-2020""")
+
+    plot = st.sidebar.selectbox('Plot:',['Notas', 'Participantes', 'Dados socioeconômicos'])
+
+    if plot == 'Notas':
+        streamlit_grade_plot()
+    if plot == 'Participantes':
+        streamlit_categorical_plot()
+    if plot == 'Dados socioeconômicos':
+        streamlit_income_plot()
+
+if __name__=='__main__':
+    main()
+
+
+
--- plot_runner.py
+++ plot_runner.py
@@ -4,15 +4,32 @@ import streamlit as st
 import geopandas as gpd
 import plotly.express as px
 import numpy as np
+import seaborn as sns
 
 
-def plot_geomap(df, var):
+def plot_geomap(df, var, cat=False):
 
     info_ufs = gpd.read_file('outils/bcim_2016_21_11_2018.gpkg', layer='lim_unidade_federacao_a')
     info_ufs.rename({'sigla': 'SG_UF_ESC'}, axis=1, inplace=True)
 
+    level_groups = {
+    1: ['A', 'B', 'C','D','E','F'],
+    2: ['G', 'H', 'I','J'],
+    3: ['K', 'L', 'M','N'],
+    4: ['O', 'P', 'Q']
+    }   
+
+    df['Q006'].replace(['A', 'B', 'C','D','E','F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P','Q'], [1,1,1,1,1,1,2,2,2,2,3,3,3,3,4,4,4], inplace=True) 
+    df['Q006'].convert_dtypes()
+
+    df["MEDIA_NOTAS"] = df[["NU_NOTA_CN", "NU_NOTA_CH", "NU_NOTA_LC", "NU_NOTA_MT", "NU_NOTA_REDACAO"]].mean(axis=1)
+
     # Agrupa os dados por estado e calcula a média das notas em cada prova
-    df_estado = df.groupby('SG_UF_ESC').mean().reset_index()
+    if not cat:
+        df_estado = df.groupby('SG_UF_ESC').mean().reset_index()
+    else:
+        df_estado = df.groupby('SG_UF_ESC').agg(pd.Series.mode)
+
     df_estado = info_ufs.merge(df_estado, on ='SG_UF_ESC', how='left')
 
     # plot_estado = df_estado.plot(column='NU_NOTA_MT', cmap='viridis', legend=True, edgecolor='black')
@@ -27,6 +44,21 @@ def plot_geomap(df, var):
                                zoom=3,
                                opacity=0.5)
     fig.update_layout(margin={'r':0,'t':0,'l':0,'b':0})
+
+    return fig
+
+def plot_violin(df, var):
+
+    
+    dados = df[["MEDIA_NOTAS", var]]
+
+    fig = sns.violinplot(data=dados, x=var, y="MEDIA_NOTAS")
+    return fig
+
+def plot_dist(df, var):
+
+    # Plotando um histograma da distribuição da média das notas
+    fig = sns.histplot(data=df, x=var, kde=True)
 
     return fig
 
