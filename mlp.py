bf1fe2b5d96d8d5c236f0b05912fdd32b862d7e8
André Meneses <andre_meneses2001@hotmail.com>
2023-07-19 15:31:22 -0300

    Add trained models

--- mlp.py
+++ mlp.py
@@ -1,22 +1,19 @@
-# Importing required libraries
 import numpy as np
 from sklearn.neural_network import MLPRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
+from data_handler_mlp import label_data
+import joblib
 
-# Function to generate synthetic data for demonstration purposes
-def generate_synthetic_data(n_samples=100):
-    np.random.seed(42)
-    X = np.linspace(-10, 10, n_samples).reshape(-1, 1)
-    y = np.sin(X) + np.random.normal(0, 0.2, n_samples).reshape(-1, 1)
-    return X, y
+def split_data(x, y, test_size=0.1):
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=test_size, random_state=42)
 
-# Function to split the data into training and testing sets
-def split_data(X, y, test_size=0.2):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
-    return X_train, X_test, y_train, y_test
+    x_train = x_train.reshape(-1,1)
+    x_test = x_test.reshape(-1,1)
 
-# Function to create the MLP model
+    return x_train, x_test, y_train, y_test
+
+
 def create_mlp(hidden_layers=(100, 100, 100, 100, 100), activation='relu', max_iter=1000):
     model = MLPRegressor(hidden_layer_sizes=hidden_layers,
                          activation=activation,
@@ -24,40 +21,54 @@ def create_mlp(hidden_layers=(100, 100, 100, 100, 100), activation='relu', max_i
                          random_state=42)
     return model
 
-# Function to train the MLP model
-def train_model(model, X_train, y_train):
-    model.fit(X_train, y_train)
+def train_model(model, x_train, y_train):
+    model.fit(x_train, y_train)
     return model
 
-# Function to evaluate the MLP model
-def evaluate_model(model, X_test, y_test):
-    y_pred = model.predict(X_test)
+def evaluate_model(model, x_test, y_test):
+    y_pred = model.predict(x_test)
     mse = mean_squared_error(y_test, y_pred)
     r2 = r2_score(y_test, y_pred)
     return mse, r2
 
+# Function to save the trained MLP model to a file
+def save_model(model, filename):
+    try:
+        joblib.dump(model, filename)
+        print("Model saved successfully as", filename)
+    except Exception as e:
+        print("Error saving the model:", e)
+
+
+# Function to load a saved MLP model from a file
+def load_model(filename):
+    try:
+        model = joblib.load(filename)
+        print("Model loaded successfully from", filename)
+        return model
+    except Exception as e:
+        print("Error loading the model:", e)
+        return None
+
 def main():
-    # Step 1: Generate synthetic data
-    X, y = generate_synthetic_data()
 
-    # Step 2: Split the data into training and testing sets
-    X_train, X_test, y_train, y_test = split_data(X, y)
+    provas = ['CN', 'MT', 'LC', 'CH']
 
-    # Step 3: Create the MLP model
-    hidden_layers = (100, 100, 100, 100, 100)
-    activation = 'relu'
-    max_iter = 1000
-    model = create_mlp(hidden_layers=hidden_layers, activation=activation, max_iter=max_iter)
+    for prova in provas:
+        x,y = label_data(prova, 'anything really')
+        x_train, x_test, y_train, y_test = split_data(x, y)
 
-    # Step 4: Train the MLP model
-    model = train_model(model, X_train, y_train)
+        model = create_mlp()
+        model = train_model(model, x_train, y_train)
 
-    # Step 5: Evaluate the MLP model
-    mse, r2 = evaluate_model(model, X_test, y_test)
+        mse, r2 = evaluate_model(model, x_test, y_test)
 
-    # Print the evaluation results
-    print("Mean Squared Error:", mse)
-    print("R-squared:", r2)
+        # Print the evaluation results
+        print("Mean Squared Error:", mse)
+        print("R-squared:", r2)
+
+        model_filename = "models/trained_model_" + prova + ".joblib"
+        save_model(model, model_filename)
 
 if __name__ == "__main__":
     main()
